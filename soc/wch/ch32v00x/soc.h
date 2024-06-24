dd6011e28262d2ef758df7f4e8b5c961cd05b0fa
Michael Hope <mlhx@google.com>
2024-06-24 20:02:14 +0200

    soc: add wch_ch32v003 stubs
    Signed-off-by: Michael Hope <mlhx@google.com>

--- soc/wch/ch32v00x/soc.h
+++ soc/wch/ch32v00x/soc.h
@@ -11,7 +11,7 @@
 #include <zephyr/types.h>
 #endif /* !_ASMLANGUAGE */
 
-#define SOC_MCAUSE_EXP_MASK          CONFIG_RISCV_SOC_MCAUSE_EXCEPTION_MASK
-#define SOC_MCAUSE_ECALL_EXP         11 /* Machine ECALL instruction */
+#define SOC_MCAUSE_EXP_MASK  CONFIG_RISCV_SOC_MCAUSE_EXCEPTION_MASK
+#define SOC_MCAUSE_ECALL_EXP 11 /* Machine ECALL instruction */
 
 #endif /* __SOC_H__ */
